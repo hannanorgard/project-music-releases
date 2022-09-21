e0ac5f677a0ae24ddb7af4afc101f4eaf02a5834
Agnes Ahlman <ahlman.agnes@gmail.com>
2022-09-21 21:25:05 +0200

    fixing error

--- code/src/App.js
+++ code/src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { AlbumComponent } from 'components/Album';
-import Header from 'components/Header'
+import Header from 'components/Header';
 
 import data from './data.json';
-
 
 console.log(data);
 
 export const App = () => {
   return (
     <>
-    <Header/>
-    <div className='grid-parent'>
-      {data.albums.items.map((singleAlbum) => (
-        <AlbumComponent key={singleAlbum.id} album={singleAlbum} />
-      ))}
-        </div>
-        </>
-    )
-    };
+      <Header />
+      <div className="grid-parent">
+        {data.albums.items.map((singleAlbum) => (
+          <AlbumComponent key={singleAlbum.id} album={singleAlbum} />
+        ))}
+      </div>
+    </>
+  );
+};
 export default App;
--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,10 +1,10 @@
-import react from 'react';
+import React from 'react';
 
 const Header = () => {
-    return (
-        <div className='headerContainer' >
-            <h1 className='headTitle'>New Album and Singles</h1>
-        </div>
-    )
+  return (
+    <div className="headerContainer">
+      <h1 className="headTitle">New Album and Singles</h1>
+    </div>
+  );
 };
-export default Header; 
+export default Header;
