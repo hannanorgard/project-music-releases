b57c3cf942ac617d311e679d8066f79f2e22fe1f
Agnes Ahlman <ahlman.agnes@gmail.com>
2022-09-24 12:14:55 +0200

    small changes in css and make albumname link

--- code/src/components/Album/index.js
+++ code/src/components/Album/index.js
@@ -5,6 +5,7 @@ import styles from './Album.module.css';
 // props = { album }
 const AlbumComponent = (props) => {
   const imgSrc = props.album.images[0];
+  console.log(props.album.external_urls);
 
   return (
     <div className="album-container">
@@ -12,20 +13,28 @@ const AlbumComponent = (props) => {
         <img className={styles.coverImg} src={imgSrc.url} alt="coverImage" />
         <Icons />
       </div>
-      <h1 className={styles.albumName}>{props.album.name}</h1>
+      <a
+        href={props.album.external_urls.spotify}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h1 className={styles.albumName}>{props.album.name}</h1>
+      </a>
+
       <div className={styles.artistContainer}>
-        {props.album.artists.map((artist, index) => {
-          return (
-            <a href={artist.external_urls.spotify}>
-              <p className={styles.artistName} key={artist.id}>
-                {artist.name}
-                <span>
-                  {props.album.artists.length - 1 > index ? ', ' : ''}
-                </span>
-              </p>
-            </a>
-          );
-        })}
+        {props.album.artists.map((artist, index) => (
+          <a
+            href={artist.external_urls.spotify}
+            key={artist.id}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className={styles.artistName}>
+              {artist.name}
+              {props.album.artists.length - 1 > index ? ', ' : ''}
+            </p>
+          </a>
+        ))}
       </div>
     </div>
   );
--- code/src/components/Header/index.js
+++ code/src/components/Header/index.js
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 const Header = () => {
   return (
     <div className={styles.headerContainer}>
-      <h1 className="headTitle">New Album and Singles</h1>
+      <h1 className={styles.headerTitle}>New Album and Singles</h1>
     </div>
   );
 };
