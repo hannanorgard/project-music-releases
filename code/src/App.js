72e902a12848edc236faa43d5db6b4b9fe91b68e
Agnes Ahlman <ahlman.agnes@gmail.com>
2022-09-24 16:52:50 +0200

    fixed header and adding hr

--- code/src/App.js
+++ code/src/App.js
@@ -7,14 +7,14 @@ console.log(data);
 
 export const App = () => {
   return (
-    <>
+    <main className="wrapper">
       <Header />
       <div className="grid-parent">
         {data.albums.items.map((singleAlbum) => (
           <AlbumComponent key={singleAlbum.id} album={singleAlbum} />
         ))}
       </div>
-    </>
+    </main>
   );
 };
 export default App;
--- code/src/components/Header/index.js
+++ code/src/components/Header/index.js
@@ -4,7 +4,8 @@ import styles from './Header.module.css';
 const Header = () => {
   return (
     <div className={styles.headerContainer}>
-      <h1 className={styles.headerTitle}>New Album and Singles</h1>
+      <h1 className={styles.headerTitle}>New Albums and Singles</h1>
+      <hr />
     </div>
   );
 };
